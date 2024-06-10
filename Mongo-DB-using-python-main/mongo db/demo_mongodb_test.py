edaabb0bd24546510dfa98f4f1c6cbcd3e17e610
HARISH AK <168578592+ak-Harish1@users.noreply.github.com>
2024-06-10 11:32:11 +0530

    Update demo_mongodb_test.py

--- Mongo-DB-using-python-main/mongo db/demo_mongodb_test.py
+++ Mongo-DB-using-python-main/mongo db/demo_mongodb_test.py
@@ -1,6 +1,6 @@
 import pymongo
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+myclient = pymongo.MongoClient("db")
 
 mydb = myclient["mydatabase"]
 print("data bases are : ",myclient.list_database_names()) 
